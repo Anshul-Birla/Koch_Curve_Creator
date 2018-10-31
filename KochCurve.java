fe7bc6f857baa7661e84040d929d48e55f3057a0
Anshul Birla <anshulbirla@gmail.com>
2018-10-31 11:44:06 -0700

    finished constructor

---
+++ KochCurve.java
@@ -0,0 +1,25 @@
+import apcslib.*;
+/**
+ * Write a description of class KochCurve here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class KochCurve extends DrawingTool
+{
+    protected double myLength;
+    protected int mySides;
+    
+    public KochCurve(double dub, int num)
+    {
+      myLength = dub;
+      mySides = num;
+    }
+    
+    public void DrawKochCurve(int sides, double length)
+    {
+        if(sides
+        
+        
+    }
+}
