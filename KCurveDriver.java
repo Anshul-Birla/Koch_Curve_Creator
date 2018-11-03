6b4b07eba36f74c3aab1ee75383c704d2bcb8ab4
Anshul Birla <anshulbirla@gmail.com>
2018-11-02 18:39:16 -0700

    started driver and finshed method

---
+++ KCurveDriver.java
@@ -0,0 +1,16 @@
+/**
+ * Write a description of class KCurveDriver here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class KCurveDriver
+{
+    public static void main(String [] args)
+    {
+        
+        
+        
+        
+    }
+}
