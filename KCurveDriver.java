964fff2cef59ca4657484bcbf4a6e63ee69dcb40
Anshul Birla <anshulbirla@gmail.com>
2018-11-02 18:43:13 -0700

    commenting in driver

--- KCurveDriver.java
+++ KCurveDriver.java
@@ -6,22 +6,21 @@
  */
 public class KCurveDriver
 {
+    
     public static void main(String [] args)
     {
-         
+        //initialize 
         KochCurve curvypen = new KochCurve();
+        
+        //this gets DrawingTool into correct position
         curvypen.up();
-         curvypen.move(-150,150);
+        curvypen.move(-150,150);
         curvypen.setDirection(0);
         curvypen.down();
-        curvypen.DrawKochCurve(6,300);
         
+        //this draws the equilateral triangle 
+        curvypen.DrawKochCurve(6,300);
         curvypen.setDirection(240);
         curvypen.DrawKochCurve(6,300);
-        curvypen.setDirection(120);
-        curvypen.DrawKochCurve(6,300);
-        
-        
-        
     }
 }
